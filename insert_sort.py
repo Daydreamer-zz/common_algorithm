44e96474391ad906e02642e50619c62e2f553da1
Daydreamer_Z <shz1997@hotmail.com>
2022-03-30 00:20:26 +0800

    insert sort demo update

--- insert_sort.py
+++ insert_sort.py
@@ -5,11 +5,11 @@ def insert_sort(li):
     length = len(li)
     for i in range(1, length): # i表示摸到的牌的下标，从1开始(因为初始手里就有第一张牌)
         tmp = li[i]  # 摸到的牌的值
-        j = i - 1 # j指的是手里牌的下标
-        while j >= 0 and li[j] > tmp:
-            li[j+1] = li[j]
+        j = i - 1 # j指的是现在手里最大值牌的下标
+        while j >= 0 and li[j] > tmp: # 手里的牌比摸到的牌的值大
+            li[j+1] = li[j] # 往右移
             j -= 1
-        li[j+1] = tmp
+        li[j+1] = tmp # 把摸到的牌插到合适位置
         print(li)
 
 
