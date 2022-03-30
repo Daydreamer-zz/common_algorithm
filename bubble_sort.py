8829cc43a8d58d2a6f7b4936ae932f46dcb08e61
Daydreamer_Z <shz1997@hotmail.com>
2022-03-30 15:54:27 +0800

    some commit

--- bubble_sort.py
+++ bubble_sort.py
@@ -5,9 +5,9 @@ import random
 
 def bubble_sort(li):
     length = len(li)
-    for i in range(length - 1): # 需要排序的数
+    for i in range(length - 1): # 需要比较的次数(因为最后一个已经是最小的，不需要再进行比较)
         exchange = False
-        for j in range(length - i - 1): # 每次对比的数
+        for j in range(length - i - 1): # 指针运行的位置，需要减去已经排序好的和最后一次不需要排序的
             if li[j] > li[j+1]:
                 li[j], li[j+1] = li[j+1], li[j]
                 exchange = True
