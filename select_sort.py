dc701b34394b900e0bdee1859e7ef8292d253506
Daydreamer_Z <shz1997@hotmail.com>
2022-03-30 11:58:05 +0800

    some commit

--- select_sort.py
+++ select_sort.py
@@ -4,8 +4,8 @@ def select_sort(li):
     length = len(li)
     for i in range(length - 1):
         min_loc = i  # 最小数的下标，初始假设一开始的
-        for j in range(i+1, length): # 关键是遍历无序区的位置
-            if li[j] < li[min_loc]:
+        for j in range(i+1, length): # 遍历无序区数字，取出最小值下标赋值给min_loc
+            if li[j] < li[min_loc]: # 依次和原最小值比较，
                 min_loc = j  # 记录无序区最小数的位置
         li[i], li[min_loc] = li[min_loc], li[i] # 交换被比较的数和最小值的位置，即把最小值放第一个
 
